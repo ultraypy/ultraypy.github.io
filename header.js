67aa78a4732c0edf83022137255ade81a235e8de
yi pei <56919476+ultraypy@users.noreply.github.com>
2025-10-13 11:13:05 +0800

    Create header.js

---
+++ header.js
@@ -0,0 +1,4 @@
+// 动态创建script标签并插入到head中
+var script = document.createElement('script');
+script.src = 'https://hm.baidu.com/hm.js?6e162fd9c3eda76fb21bd3c5b14abd84'; // 替换为你的统计ID
+document.head.appendChild(script);
